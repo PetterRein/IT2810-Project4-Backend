e57435dc41e6d78d26d51b1e77ae3a0f95d89811
Petter Grø Rein <pettegre@stud.ntnu.no>
2019-11-13 15:22:45 +0100

    add back first and skip #3

--- src/resolvers/Movies.js
+++ src/resolvers/Movies.js
@@ -3,19 +3,19 @@ import Movie from '../models/Movie';
 export default function getMoviesFromDB(args) {
   if (!args.vote_average) {args.vote_average = 0.0}
   if(args.filter && args.sortDir){
-    const movies = Movie.find({ $text: { $search: args.filter }, vote_average: {$gte: args.vote_average}}).sort('-'+args.sortField)
+    const movies = Movie.find({ $text: { $search: args.filter }, vote_average: {$gte: args.vote_average}}).sort('-'+args.sortField).skip(args.skip).limit(args.first)
     return movies
   }
   else if (args.filter) {
-    const movies = Movie.find({ $text: { $search: args.filter }, vote_average: {$gte: args.vote_average}}).sort(args.sortField)
+    const movies = Movie.find({ $text: { $search: args.filter }, vote_average: {$gte: args.vote_average}}).sort(args.sortField).skip(args.skip).limit(args.first)
     return movies
   }
   else if (args.sortDir){
-    const movies = Movie.find({vote_average: {$gte: args.vote_average}}).sort('-'+args.sortField)
+    const movies = Movie.find({vote_average: {$gte: args.vote_average}}).sort('-'+args.sortField).skip(args.skip).limit(args.first)
     return movies
   }
   else {
-    const movies = Movie.find({vote_average: {$gte: args.vote_average}}).sort(args.sortField)
+    const movies = Movie.find({vote_average: {$gte: args.vote_average}}).sort(args.sortField).skip(args.skip).limit(args.first)
     return movies
   }
 }
